0c4f01696c2c3452cf3dc82a0ec4ae816b2faf43
Pluem <papop.ru@mail.kmutt.ac.th>
2020-05-06 16:50:20 +0700

    update add function updateShop, deleteByShopId

--- disefood-api/app/Http/Controllers/ShopController.php
+++ disefood-api/app/Http/Controllers/ShopController.php
@@ -6,6 +6,7 @@ use App\Repositories\Interfaces\ShopRepositoryInterface;
 use App\Repositories\Interfaces\FoodRepositoryInterface;
 use App\Http\Requests\CreateShopRequest;
 use App\Http\Requests\CreateFoodRequest;
+use App\Http\Requests\UpdateShopRequest;
 use Illuminate\Support\Facades\Storage;
 
 class ShopController extends Controller
@@ -51,5 +52,25 @@ class ShopController extends Controller
         $path = Storage::disk('s3')->put('images/shop/food/cover_image', $request->file('cover_image'));
         $food['cover_image'] = $path;
         return $this->foodRepo->addFood($food, $shop_id);
+    }
+
+    public function updateShop(UpdateShopRequest $request, $shop_id)
+    {
+        $shop = $request->validated();
+//        $shopBeforeUpdate = $this->shopRepo->findById($shop_id);
+//        Storage::delete($shopBeforeUpdate['cover_image']);
+//        $shop['cover_image'] = Storage::disk('s3')->put('images/shop/food/cover_image', $request->file('cover_image'));
+        $this->shopRepo->updateShop($shop, $shop_id);
+        $shop = $this->shopRepo->findById($shop_id);
+        return response()->json($shop);
+    }
+
+    public function deleteByShopId($shop_id)
+    {
+        $shopBeforeDelete = $this->shopRepo->findById($shop_id);
+        $path = $shopBeforeDelete['cover_image'];
+        Storage::disk('s3')->delete($path);
+        $this->shopRepo->delete($shop_id);
+        return response('delete success', 200);
     }
 }
