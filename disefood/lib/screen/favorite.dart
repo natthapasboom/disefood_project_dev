647ab4750d787bdca4b99becc158b7e91d56db02
Tae Nawapan <nawapan.dee@pandasoft.co.th>
2020-11-22 11:21:59 +0700

    add onTap in fav

--- disefood/lib/screen/favorite.dart
+++ disefood/lib/screen/favorite.dart
@@ -81,106 +81,111 @@ class _FavoritePageState extends State<FavoritePage> {
                             return Container(
                               margin: EdgeInsets.only(
                                   bottom: 0, top: 20, left: 40, right: 40),
-                              child: Card(
-                                semanticContainer: true,
-                                clipBehavior: Clip.antiAliasWithSaveLayer,
-                                elevation: 5,
-                                color: Colors.white70,
-                                // margin: EdgeInsets.only(
-                                //     top: 8, bottom: 8, left: 40, right: 40),
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    CachedNetworkImage(
-                                      imageUrl:
-                                          "https://disefood.s3-ap-southeast-1.amazonaws.com/" +
-                                              "${data.shop.coverImg}",
-                                      width: 380,
-                                      height: 140,
-                                      fit: BoxFit.cover,
-                                      placeholder: (context, url) => Center(
-                                          child: Container(
-                                              margin: EdgeInsets.only(
-                                                  top: 50, bottom: 35),
-                                              child: CircularProgressIndicator(
-                                                strokeWidth: 5.0,
-                                                valueColor:
-                                                    AlwaysStoppedAnimation(
-                                                        const Color(
-                                                            0xffF6A911)),
-                                              ))),
-                                      errorWidget: (context, url, error) =>
-                                          Container(
-                                        height: 140,
+                              child: InkWell(
+                                onTap: () {},
+                                child: Card(
+                                  semanticContainer: true,
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  elevation: 5,
+                                  color: Colors.white70,
+                                  // margin: EdgeInsets.only(
+                                  //     top: 8, bottom: 8, left: 40, right: 40),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      CachedNetworkImage(
+                                        imageUrl:
+                                            "https://disefood.s3-ap-southeast-1.amazonaws.com/" +
+                                                "${data.shop.coverImg}",
                                         width: 380,
-                                        color: const Color(0xff7FC9C5),
-                                        child: Center(
-                                          child: Icon(
-                                            Icons.store,
-                                            size: 50,
-                                            color: Colors.white,
+                                        height: 140,
+                                        fit: BoxFit.cover,
+                                        placeholder: (context, url) => Center(
+                                            child: Container(
+                                                margin: EdgeInsets.only(
+                                                    top: 50, bottom: 35),
+                                                child:
+                                                    CircularProgressIndicator(
+                                                  strokeWidth: 5.0,
+                                                  valueColor:
+                                                      AlwaysStoppedAnimation(
+                                                          const Color(
+                                                              0xffF6A911)),
+                                                ))),
+                                        errorWidget: (context, url, error) =>
+                                            Container(
+                                          height: 140,
+                                          width: 380,
+                                          color: const Color(0xff7FC9C5),
+                                          child: Center(
+                                            child: Icon(
+                                              Icons.store,
+                                              size: 50,
+                                              color: Colors.white,
+                                            ),
                                           ),
                                         ),
                                       ),
-                                    ),
-                                    Container(
-                                      color: Colors.grey[50],
-                                      child: ListTile(
-                                        title: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Row(
-                                              children: [
-                                                Text(
-                                                  "0.${data.shopId}",
-                                                  style: TextStyle(
-                                                      fontSize: 16,
-                                                      color: Colors.black,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                ),
-                                                Container(
-                                                  height: 20,
-                                                  child: VerticalDivider(
-                                                    color: Colors.black38,
-                                                    thickness: 3,
+                                      Container(
+                                        color: Colors.grey[50],
+                                        child: ListTile(
+                                          title: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Row(
+                                                children: [
+                                                  Text(
+                                                    "0.${data.shopId}",
+                                                    style: TextStyle(
+                                                        fontSize: 16,
+                                                        color: Colors.black,
+                                                        fontWeight:
+                                                            FontWeight.bold),
                                                   ),
-                                                ),
-                                                Text(
-                                                  "${data.shop.name}",
-                                                  style: TextStyle(
-                                                      fontSize: 16,
-                                                      color: Colors.black,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                ),
-                                                Container(
-                                                  height: 20,
-                                                  child: VerticalDivider(
-                                                    color: Colors.black38,
-                                                    thickness: 3,
+                                                  Container(
+                                                    height: 20,
+                                                    child: VerticalDivider(
+                                                      color: Colors.black38,
+                                                      thickness: 3,
+                                                    ),
                                                   ),
-                                                ),
-                                              ],
-                                            ),
-                                          ],
+                                                  Text(
+                                                    "${data.shop.name}",
+                                                    style: TextStyle(
+                                                        fontSize: 16,
+                                                        color: Colors.black,
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                  ),
+                                                  Container(
+                                                    height: 20,
+                                                    child: VerticalDivider(
+                                                      color: Colors.black38,
+                                                      thickness: 3,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                          // subtitle: Row(
+                                          //   children: <Widget>[
+                                          //     Icon(
+                                          //       Icons.star,
+                                          //       color: Colors.orange,
+                                          //     ),
+                                          //     Text("  4.2 Review(20 Review)")
+                                          //   ],
+                                          // ),
                                         ),
-                                        // subtitle: Row(
-                                        //   children: <Widget>[
-                                        //     Icon(
-                                        //       Icons.star,
-                                        //       color: Colors.orange,
-                                        //     ),
-                                        //     Text("  4.2 Review(20 Review)")
-                                        //   ],
-                                        // ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
 //          crossAxisAlignment: CrossAxisAlignment.start,
+                                  ),
                                 ),
                               ),
                             );
