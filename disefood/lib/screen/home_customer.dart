bdc0794c2d7a3d89f382ed1906c0926012b8a953
Tae Nawapan <nawapan.dee@pandasoft.co.th>
2020-11-22 21:18:29 +0700

    delete cloud, edit icon star

--- disefood/lib/screen/home_customer.dart
+++ disefood/lib/screen/home_customer.dart
@@ -387,7 +387,7 @@ class _HomeState extends State<Home> {
                                                               EdgeInsets.only(
                                                                   top: 1),
                                                           child: Icon(
-                                                            Icons.star_rounded,
+                                                            Icons.star,
                                                             color:
                                                                 Colors.orange,
                                                           ),
@@ -413,7 +413,7 @@ class _HomeState extends State<Home> {
                                                               EdgeInsets.only(
                                                                   top: 1),
                                                           child: Icon(
-                                                            Icons.star_rounded,
+                                                            Icons.star,
                                                             color:
                                                                 Colors.orange,
                                                           ),
